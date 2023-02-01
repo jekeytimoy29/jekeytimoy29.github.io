9e12bcad1a3f6e7adafc2722db3707d6abda3fd0
Timothy Jake Flordelis <mothj20@gmail.com>
2023-02-01 09:56:09 -0600

    Added new changes for the Maze Game functionalities

--- scripts/maze.js
+++ scripts/maze.js
@@ -5,15 +5,25 @@ let playOn = false;
 $(mainFunction);
 
 function mainFunction() {
-  $(".boundary").mouseover(() => {
-    if (playOn) {
-      $(".boundary").addClass("youlose");
-      playOn = false;
-    }
-  });
+  $(".boundary")
+    .mouseover(() => {
+      if (playOn) {
+        $(".boundary").addClass("youlose");
+        $("#status").text("You Lose! :(");
+        playOn = false;
+      }
+    })
+    .mouseleave(() => $("#status").text("Click the 'S' to begin."));
 
   $("#start").click(() => {
     playOn = true;
     $(".boundary").removeClass("youlose");
   });
+
+  $("#end")
+    .mouseover(() => {
+      $("#status").text("You Win! :)");
+      playOn = false;
+    })
+    .mouseleave(() => $("#status").text("Click the 'S' to begin."));
 }
