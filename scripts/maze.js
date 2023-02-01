10c7180f383964035fcb78361567753b84470336
Timothy Jake Flordelis <mothj20@gmail.com>
2023-02-01 13:12:23 -0600

    Fixed issues found in the Maze game

--- scripts/maze.js
+++ scripts/maze.js
@@ -5,25 +5,35 @@ let playOn = false;
 $(mainFunction);
 
 function mainFunction() {
-  $(".boundary")
-    .mouseover(() => {
-      if (playOn) {
-        $(".boundary").addClass("youlose");
-        $("#status").text("You Lose! :(");
-        playOn = false;
-      }
-    })
-    .mouseleave(() => $("#status").text("Click the 'S' to begin."));
-
-  $("#start").click(() => {
-    playOn = true;
-    $(".boundary").removeClass("youlose");
+  $(".boundary").mouseover(() => {
+    if (playOn) {
+      $(".boundary").addClass("youlose");
+      $("#status").text("You Lose! :(");
+      playOn = false;
+    }
   });
 
-  $("#end")
-    .mouseover(() => {
+  $("#start").click(() => (playOn = true));
+
+  $("#end").mouseover(() => {
+    if (playOn) {
       $("#status").text("You Win! :)");
       playOn = false;
-    })
-    .mouseleave(() => $("#status").text("Click the 'S' to begin."));
+    }
+  });
+
+  $("#maze").mouseleave((e) => {
+    if (playOn) {
+      $(".boundary").addClass("youlose");
+      $("#status").text("You Lose! :(");
+      playOn = false;
+    }
+  });
+
+  $("#maze")
+    .parent()
+    .click(() => {
+      $("#status").text('Click the "S" to begin.');
+      $(".boundary").removeClass("youlose");
+    });
 }
